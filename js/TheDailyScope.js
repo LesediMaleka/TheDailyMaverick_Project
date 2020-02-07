f4b62505c81deeffc82408827b64139d6650f8ff
LesediMaleka <lesedi@adadev.org.za>
2020-02-07 08:58:35 +0200

    renamed the pics,added the alt tags,added meta data

--- js/TheDailyScope.js
+++ js/TheDailyScope.js
@@ -1,3 +1,52 @@
+
+
+    /* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+  //this is the subscribe section
+  const subscribebtn = document.querySelector('.subscribe');
+  const wrapper = document.querySelector('.popupWrapper')
+  const close = document.querySelector('.close');
+  const signmeup = document.querySelector('#signmeup');
+  const privacy = document.querySelector('#Privacy');
+ 
+  
+  
+   subscribebtn.addEventListener('click', () =>{
+       wrapper.style.display = 'block'
+  
+   });    
+   
+   close.addEventListener('click', () =>{
+    wrapper.style.display = 'none'
+   });
+  
+   const pW = document.querySelector('.popupWrapper');
+  
+   pW.addEventListener('click',  e=>{
+      
+      if(e.target.className === 'popupWrapper'){
+          wrapper.style.display = 'none';
+      }
+  
+     });
+  
+  signmeup.addEventListener('click', () =>{
+      window.open('https://www.dailymaverick.co.za/about/newsletter/')
+  });
+  privacy.addEventListener('click', () =>{
+      window.open('https://www.dailymaverick.co.za/privacy-policy/')
+  
+  });
+ 
+
+  
   //this is the footer section
   const facebook = document.querySelector('.facebook')
   const instagram = document.querySelector('.instagram')
