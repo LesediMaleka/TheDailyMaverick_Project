500ccc82a569bc7ccd6fd9160835ce52bc2b463d
LesediMaleka <lesedi@adadev.org.za>
2020-02-12 14:07:33 +0200

    added the create the new template

--- js/TheGalesOfLaughter.js
+++ js/TheGalesOfLaughter.js
@@ -1,33 +1,4 @@
-const subscribe = document.querySelector('.subscribeButton');
-const wrapper = document.querySelector('.popupWrapper');
-const close = document.querySelector('.close');
 
-
-subscribe.addEventListener('click', () =>{
-    wrapper.style.display = 'block'
-
-});   
-close.addEventListener('click', () =>{
-    wrapper.style.display = 'none'
-   });
-
-   signmeup.addEventListener('click', () =>{
-    window.open('https://www.dailymaverick.co.za/about/newsletter/')
-});
-privacy.addEventListener('click', () =>{
-    window.open('https://www.dailymaverick.co.za/privacy-policy/')
-
-});
-
-
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
 
 
   
