d451b1003f3e43960d16e0189b4149d43587c4c3
TshepoMakgetsi99 <tshepo@adadev.org.za>
2020-02-20 12:17:04 +0200

    nav

--- js/template.js
+++ js/template.js
@@ -1,39 +1,39 @@
-// function openNav() {
-//   document.getElementById("myNav").style.left = "0";
-// }
+function openNav() {
+  document.getElementById("myNav").style.left = "0";
+}
 
-// /* Close when someone clicks on the "x" symbol inside the overlay */
-// function closeNav() {
-//   document.getElementById("myNav").style.left = "-100vw";
-// }
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.left = "-100vw";
+}
 
 //   //this is the subscribe section
-//   const subscribebtn = document.querySelector('.ButtonSubscribe');
-//   const wrapper = document.querySelector('.popupWrapper')
-//   const close = document.querySelector('.close');
-//   // const signmeup = document.querySelector('#signmeup');
-//   // const privacy = document.querySelector('#Privacy');
+  const subscribebtn = document.querySelector('.ButtonSubscribe');
+  const wrapper = document.querySelector('.popupWrapper')
+  const close = document.querySelector('.close');
+  // const signmeup = document.querySelector('#signmeup');
+  // const privacy = document.querySelector('#Privacy');
  
   
   
-//    subscribebtn.addEventListener('click', () =>{
-//        wrapper.style.display = 'block'
+   subscribebtn.addEventListener('click', () =>{
+       wrapper.style.display = 'block'
   
-//    });    
+   });    
    
-//    close.addEventListener('click', () =>{
-//     wrapper.style.display = 'none'
-//    });
+   close.addEventListener('click', () =>{
+    wrapper.style.display = 'none'
+   });
   
-//    const pW = document.querySelector('.popupWrapper');
+   const pW = document.querySelector('.popupWrapper');
   
-//    pW.addEventListener('click',  e=>{
+   pW.addEventListener('click',  e=>{
       
-//       if(e.target.className === 'popupWrapper'){
-//           wrapper.style.display = 'none';
-//       }
+      if(e.target.className === 'popupWrapper'){
+          wrapper.style.display = 'none';
+      }
   
-//      });
+     });
   
 
   signmeup.addEventListener('click', () =>{
